9f4c5b9c69f149e7d9ee016569861be935d220c8
Marcelo Dias <marcelo.sdias@inf.ufpel.edu.br>
2020-10-14 16:09:25 -0300

    Adicionando exercicio da segunda semana

---
+++ Atividades da Segunda Semana/slide28.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char nome[30];
+    int idade;
+    int altura;
+} Pessoa;
+
+int main () {
+    Pessoa *p;
+
+    int i, index = 0, tamanho = 1, opcao;
+
+    p = (Pessoa*)malloc(sizeof(Pessoa));
+    do {
+        printf("Informe o nome: ");
+        scanf("%[^\n]s", p[index].nome);
+
+        printf("Informe a idade: ");
+        scanf("%d", &p[index].idade);
+
+        printf("Informe a altura: ");
+        scanf("%d", &p[index].altura);
+
+        printf("Adicionar outro registro (1.Sim 2.Nao): ");
+        scanf("%d", &opcao);
+        getchar();
+
+        if (opcao == 1) {
+            p = (Pessoa*)realloc(p, ++tamanho * sizeof(Pessoa));
+            index++;
+        }
+    } while (opcao != 2);
+
+    for (i = 0; i <= index; i++) {
+        printf("Nome: %s; Idade: %d; Altura: %d\n", p[i].nome, p[i].idade, p[i].altura);
+    }
+
+    free(p);
+
+    return 0;
+}
